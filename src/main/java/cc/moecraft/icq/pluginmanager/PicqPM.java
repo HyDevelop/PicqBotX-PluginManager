7a9b3347af39b400ef9f73bf16cc9a6977184047
Hykilpikonna <admin@moecraft.cc>
2018-07-24 20:19:15 +0800

    [+] 静态方法类

---
+++ src/main/java/cc/moecraft/icq/pluginmanager/PicqPM.java
@@ -0,0 +1,20 @@
+package cc.moecraft.icq.pluginmanager;
+
+import cc.moecraft.icq.pluginmanager.plugin.IcqPlugin;
+import cc.moecraft.icq.pluginmanager.plugin.PluginManager;
+import cc.moecraft.logger.LoggerInstanceManager;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/07/15 创建!
+ * Created by Hykilpikonna on 2018/07/15!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class PicqPM
+{
+}
